d09ca99387547112dedf3cba0432da2399911c5e
XXXiaoyan <li2185@purdue.edu>
2018-12-05 05:05:18 -0500

    testEB

--- theIntrudersGameTest/testnode/testDeploy/app.js
+++ theIntrudersGameTest/testnode/testDeploy/app.js
@@ -1,7 +1,8 @@
 var http = require('http');
 var dt = require('./myfirstmodule');
 var fs = require('fs');
-
+var AWS = require('aws-sdk');
+AWS.config.region = process.env.REGION
 http.createServer(function (req, res) {
   fs.readFile('demofile1.html', function(err, data) {
     res.writeHead(200, {'Content-Type': 'text/html'});
