d9bc6600c5b4864e21d4e0029cf22232ae15197f
XXXiaoyan <li2185@purdue.edu>
2018-12-06 02:39:53 +0000

    app3

--- theIntrudersGameTest/testSocket/app2.js
+++ theIntrudersGameTest/testSocket/app2.js
@@ -6,16 +6,36 @@ app.get('/', function(req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
+var counter = -1;
+var array = [];
+var uc = [0]; // update counter
 io.on('connection', function(socket) {
   console.log('An user connected.');
+  
+  socket.on('request', function(msg) {
+    console.log('An user sent a request');
+    counter++;
+    socket.emit('accept', counter);
+    console.log('new userid: ' + counter);
+  });  
+  
   socket.on('update', function(msg) {
-    var pos = msg;
-    console.log('x: ' + pos.x + '\t\t\ty:' + pos.y);
-    io.emit('update', msg);
+    //console.log('x: ' + msg.x + '\t\t\ty: ' + msg.y + '\t\t\tid: ' + msg.id);
+    console.log('msg: ' + msg.x + msg.y + msg.id);
+    array.push(msg);
+    uc[msg.id] = 1;
+    if (uc == counter) {
+      console.log('array: ' + array[0].x + array[0].y + array[0].id);
+      if (counter >= 1) console.log('\t' +array[1].x + array[1].y + array[1].id);
+      io.emit('update', array);
+      uc = 0;
+    }
   });
+  
   socket.on('disconnect', function() {
     console.log('user disconnected');
   });
+
 });
 
 http.listen(3000, function() {
