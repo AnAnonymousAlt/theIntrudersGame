52d9a6e354ac5cd0c713e93f28e91a4ba6c79b82
XXXiaoyan <li2185@purdue.edu>
2018-12-06 04:22:00 +0000

    demo_completed

--- theIntrudersGameTest/testSocket/app2.js
+++ theIntrudersGameTest/testSocket/app2.js
@@ -8,8 +8,9 @@ app.get('/', function(req, res) {
 
 var counter = -1;
 var array = [];
-var uc = [0]; // update counter
-io.on('connection', function(socket) {
+var uc = []; // update counter
+var ucc = -1;
+io.sockets.on('connection', function(socket) {
   console.log('An user connected.');
   
   socket.on('request', function(msg) {
@@ -17,22 +18,44 @@ io.on('connection', function(socket) {
     counter++;
     socket.emit('accept', counter);
     console.log('new userid: ' + counter);
+    uc.push(0);
   });  
   
   socket.on('update', function(msg) {
     //console.log('x: ' + msg.x + '\t\t\ty: ' + msg.y + '\t\t\tid: ' + msg.id);
-    console.log('msg: ' + msg.x + msg.y + msg.id);
-    array.push(msg);
-    uc[msg.id] = 1;
-    if (uc == counter) {
-      console.log('array: ' + array[0].x + array[0].y + array[0].id);
+    console.log('msg: ' + msg.x + ' ' + msg.y + ' '  + msg.id);
+    console.log('uc: ' + uc + 'uc[0]: ' + uc[0]);
+    console.log('ucc: ' + ucc);
+    console.log('counter: ' + counter);
+    if (uc[msg.id] == 0) {
+      array.push(msg);
+      uc[msg.id] = 1;
+      ucc = ucc + 1;
+    }
+    console.log('');
+    console.log('msg: ' + msg.x + ' ' + msg.y + ' '  + msg.id);
+
+    console.log('uc: ' + uc + 'uc[0]: ' + uc[0]);
+    console.log('ucc: ' + ucc);
+    console.log('counter: ' + counter);
+		    
+    console.log('-------------');
+    if (ucc == counter && ucc != -1) {
+      console.log('array: ' + array[0].x + ' ' + array[0].y + ' ' + array[0].id);
       if (counter >= 1) console.log('\t' +array[1].x + array[1].y + array[1].id);
       io.emit('update', array);
-      uc = 0;
+      var i = 0;
+      while (i <= counter) {
+	uc[i] = 0;
+	i++;
+      }
+      array = [];
+      ucc = -1;
     }
-  });
+});
   
   socket.on('disconnect', function() {
+    counter--;
     console.log('user disconnected');
   });
 
