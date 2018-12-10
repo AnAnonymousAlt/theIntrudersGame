03209c9ca5780e24898be36b45adc7e89ec5f998
XXXiaoyan <li2185@purdue.edu>
2018-12-10 04:46:21 -0500

    rewrtiting

--- playground/socketTest/app.js
+++ playground/socketTest/app.js
@@ -10,10 +10,38 @@ server.listen(3000, function() {
 
 app.get("/", function(req, res) {
   res.sendFile(__dirname + "/game.html");
-  io.on("connection", function(socket) {
-  });  
 });
 
+app.get("/canvas", function(req, res) {
+  res.sendFile(__dirname + "/gamecanvas.html");
+});
 app.get("/pic/\*", function(req,res) {
   res.sendFile(__dirname + req.path);
+});
+
+
+var update_message = {
+  player1 : -1,
+  player2 : -1,
+  player1x : -1,
+  player1y : -1,
+  player2x : -1,
+  player2y : -1
+};
+var player_counter = 0;
+var update_counter = 0;
+
+io.on("connection", function(socket) {
+  console.log("get connection");
+  socket.on("gamestart", function(msg) {
+    console.log("gamestart");
+    socket.emit("approve", player_counter);
+    player_counter++;
+  });
+  socket.on("update", function(msg) {
+    console.log("update");
+  });
+  socket.on("disconnect", function(msg) {
+    console.log("diconnection");
+  })
 });
